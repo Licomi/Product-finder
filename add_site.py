a76db8ca737881aacb8d28543cb9b1cb9c981805
Licomi <ivandvoretskiy5@gmail.com>
2021-12-04 21:21:36 +0700

    Добавлена первая версия функции поиска товара, и определения продавца (осталось получить со станици продавца кол. пописчиков и рейтинг). В БД мелкие правки в таблицу Sellers и Items

--- add_site.py
+++ add_site.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, sqlite3, re
+import requests, sqlite3, re, datetime
 # import SQL_query
 
 
@@ -12,9 +12,11 @@ def get_icon_site(url: str) -> str:
         icon_url = url + icon_url
     return requests.get(icon_url).content
 
-def add_categories(DB_name: str, list_category, show_result: bool = True) -> None:
+
+def add_categories(DB_name: str, list_category, id_shop, show_result: bool = True) -> None:
     conn = sqlite3.connect(DB_name)
     cursor = conn.cursor()
+
     for i_c, category in enumerate(list_category):
         cursor.execute(""" INSERT INTO Categories(id_shop, name, url)
                         VALUES(?, ?, ?)""", (id_shop, category[0], category[1]))
@@ -28,28 +30,61 @@ def add_categories(DB_name: str, list_category, show_result: bool = True) -> Non
                             ON c.id = sc.id_category""")
         for i in cursor.fetchall():
             print(i)
+
     conn.commit()
     conn.close()
 
+def parse_category_AliExpress(url: str):
+    # Поиск категорий AliExpress
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
+    list_catigory_soup = soup.find_all('h3', {'class': 'big-title anchor1 anchor-agricuture'} )
+    list_sub_catigory_soup = soup.find_all('ul', {'class': 'sub-item-cont util-clearfix'})
+
+    # Формирование списка категорий AliExpress
+    lsit_shop_category = []
+    for i_category, category in enumerate(list_catigory_soup):
+        list_sub_catigory = []
+        for li in list_sub_catigory_soup[i_category]:
+            if li.name != None:
+                list_sub_catigory.append((li.a.text, 'https:' + li.a.get("href")))
+        lsit_shop_category.append((category.a.text, 'https:' + category.a.get('href'), list_sub_catigory))
+    return(lsit_shop_category)
+
 
 URLS = ['https://aliexpress.ru/all-wholesale-products.html', 'https://www.ozon.ru', 'https://www.wildberries.ru', 'https://alexgyver.ru', 'https://arduino54.ru']
-id_shop = 1
+
+def search_item_AliExpress(item: str, category: int = None, w_db: bool = False, mode: int = 0) -> list:
+    item = '%20'.join(item.split(' '))
+    soup = BeautifulSoup(requests.get('https://aliexpress.ru/wholesale?SearchText=' + item).text, 'lxml')
+    count_items = soup.find_all('div', {'class': 'SearchProductFeed_HorizontalCard__card__102el SearchProductFeed_Preview__card__3zxie'})
+
+    # Информация о продавце
+    conn = sqlite3.connect("DATABASE.db")
+    cursor = conn.cursor()
+    cursor.execute("""SELECT id_site FROM Sellers""")
+    list_sellers = cursor.fetchall()
+    
+    for i in count_items:
+        lsit_new_sellers = []
+        a_seller = i.find('a', {'href': re.compile('//aliexpress.ru/store/')})
+        id_site = a_seller.get('href').split('/')[-1]
+        if id_site not in list_sellers:
+            print('https:' + a_seller.get('href'))
+            print(requests.get('https:' + a_seller.get('href')).text)
+            break
+            # s_soup = BeautifulSoup(requests.get('https:' + a_seller.get('href')).text, 'lxml')
+            # for j in s_soup.find_all('span'):
+            #     print(j.text)
+            
+            # list_sellers.append()
 
 
-# Поиск категорий AliExpress
-soup = BeautifulSoup(requests.get(URLS[0]).text, 'lxml')
+search_item_AliExpress('arduino nano')
 
-list_catigory_soup = soup.find_all('h3', {'class': 'big-title anchor1 anchor-agricuture'} )
-list_sub_catigory_soup = soup.find_all('ul', {'class': 'sub-item-cont util-clearfix'})
 
-# Формирование списка категорий
-lsit_shop_category = []
-for i_category, category in enumerate(list_catigory_soup):
-    list_sub_catigory = []
-    for li in list_sub_catigory_soup[i_category]:
-        if li.name != None:
-            list_sub_catigory.append((li.a.text, 'https:' + li.a.get("href")))
-    lsit_shop_category.append((category.a.text, 'https:' + category.a.get('href'), list_sub_catigory))
+
+
+    
 
     # print(i_c, category[0], category[1])
     # print(category[2])
